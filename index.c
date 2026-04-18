05fbff1b711b919fe79b393a666c5ac3dc829395
Bhuvi Bagga <bhuvibagga1221@gmail.com>
2026-04-18 08:22:39 +0530

    Phase 3: Add skeleton for index functions

--- index.c
+++ index.c
@@ -135,12 +135,16 @@ int index_status(const Index *index) {
 //
 // Returns 0 on success, -1 on error.
 int index_load(Index *index) {
-    // TODO: Implement index loading
-    // (See Lab Appendix for logical steps)
-    (void)index;
     return -1;
 }
 
+int index_save(const Index *index) {
+    return -1;
+}
+
+int index_add(Index *index, const char *path) {
+    return -1;
+}
 // Save the index to .pes/index atomically.
 //
 // HINTS - Useful functions and syscalls:
@@ -151,12 +155,6 @@ int index_load(Index *index) {
 //   - rename                           : atomically moving the temp file over the old index
 //
 // Returns 0 on success, -1 on error.
-int index_save(const Index *index) {
-    // TODO: Implement atomic index saving
-    // (See Lab Appendix for logical steps)
-    (void)index;
-    return -1;
-}
 
 // Stage a file for the next commit.
 //
@@ -167,9 +165,3 @@ int index_save(const Index *index) {
 //   - index_find                       : checking if the file is already staged
 //
 // Returns 0 on success, -1 on error.
-int index_add(Index *index, const char *path) {
-    // TODO: Implement file staging
-    // (See Lab Appendix for logical steps)
-    (void)index; (void)path;
-    return -1;
-}
